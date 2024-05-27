d1d7a76de5cdb4f5df4602da4fd3476c32e7e3cd
Rahul Chauhan <omrahulchauhan@gmail.com>
2024-05-27 22:02:35 +0200

    Initial Commit

---
+++ examples/helloWorld/hello.lua
@@ -0,0 +1,16 @@
+--[[
+As customary; Lets write a helloworld program in lua
+]] 
+ 
+print('Lua: Hello World!')
+
+print("Earth: Hello Moon!")
+
+--[[
+To run the script
+```
+❯ lua hello.lua
+Lua: Hello World!
+Earth: Hello Moon!
+```
+]]
